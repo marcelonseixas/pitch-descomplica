ef3da9dc4848c0729c44b9876b78189d05703e4e
M Seixas <marcnascseixas@gmail.com>
2023-12-05 10:26:13 -0300

    05-12-23
    Correção código

--- Hello.py
+++ Hello.py
@@ -14,14 +14,59 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
-
+import requests
 LOGGER = get_logger(__name__)
 
+def Extractor():
+        endpoint_descomplica = 'https://flowpress.prd.descomplica.com.br/graduacao/wp-json/wp/v2/'
+        metadata = []
+        cat_pos = f'{endpoint_descomplica}cat_pos_graduacao?per_page=99&orderby=name&order=asc'
+
+        response_cat_pos = requests.get(cat_pos)
+        if response_cat_pos.status_code == 200:
+            content_cat_pos = response_cat_pos.json()
+            for cat in content_cat_pos:
+                id_cat = cat['id']
+                name_cat = cat['name']
+                course_count = cat['course_count']
+
+                url_course = f'{endpoint_descomplica}pos_graduacao_search/?cat_pos_graduacao={id_cat}'
+                respose_menu = requests.get(url_course)
+                list_courses = respose_menu.json()
+                for course in list_courses:
+                    id_course = course['id']
+                    title = course['title']
+                    type_ = course['type']
+                    status = course['status']
+                    old_price = course['acf']['courseOldPrice']
+                    current_price = course['acf']['courseCurrentPrice']
+                    current_price1 = course['acf']['courseCurrentPrice1']
+                    courseDescription = course['acf']['courseDescription']
+                    courseBestsellerIndex = course['acf']['courseBestsellerIndex']
+                    courseNumberOfPayments = course['acf']['courseNumberOfPayments']
+                    data = {
+                        "ID CATEGORIA":int(id_cat),
+                        "CATEGORIA":name_cat,
+                        "VOLUME CATEGORIA":course_count,
+                        "ID CURSO":int(id_course),
+                        "TITULO":title,
+                        "TIPO":str(type_).replace('_',' '),
+                        "STATUS":status,
+                        "VALOR ANTERIOR":old_price,
+                        "VALOR ATUAL":current_price,
+                        "AJUSTE ANTERIOR":current_price1,
+                        "MAIS VENDIDO":courseBestsellerIndex,
+                        "PARCELAMENTO":courseNumberOfPayments,
+                        "DESCRICAO":courseDescription
+
+                    }
+                    metadata.append(data)
+            return metadata
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Pitcha Descomplica",
+        page_icon=":bookmark_tabs:",
     )
 
     st.write("# Welcome to Streamlit! 👋")
